5f964963775a6f330adb439d10eaa17b520d713d
dazzm <mazam@pipingrock.com>
2018-10-12 00:09:36 +0530

    counter

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,11 +1,10 @@
 import ajax from './utils/index'
-//import fetch from 'node-fetch'
+
+let counter = 0
 
 exports.handler = async (event, context) => {
     const url = "https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke"
-    // let res = await fetch(url)
-    // let data = await res.json()
     let data = await ajax.getJson(url)
-    let response = { statusCode: 200, body: JSON.stringify({msg: "hi", ...data}) }
+    let response = { statusCode: 200, body: JSON.stringify({msg: "hi: "+ ++counter, ...data}) }
     return response
 }
