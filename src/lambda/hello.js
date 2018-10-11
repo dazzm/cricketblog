d6f3d932184f97d5508876a39bbe5d8e77c2daa3
dazzm <mazam@pipingrock.com>
2018-10-12 02:17:50 +0530

    replaced node-fetch with axios

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,12 +1,12 @@
 import cache from 'simplecaching'
-import ajax from './utils/index'
+import axios from 'axios'
 
 let counter = 1, msg = "hello "
 cache.set("counter", counter, 5*60)
 
 exports.handler = async (event, context) => {
     const url = "https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke"
-    let data = await ajax.getJson(url)
+    let data = await axios.get(url)
     let cout = cache.get("counter", 0)
     let response = { statusCode: 200, body: JSON.stringify({msg: msg+cout, ...data}) }
     return response
