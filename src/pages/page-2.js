0b0c09f859ea2907f40843265a33dcfaf762b6b6
dazzm <mazam@pipingrock.com>
2018-10-11 20:26:29 +0530

    added lambda function hello

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -10,4 +10,23 @@ const SecondPage = () => (
   </Layout>
 )
 
-export default SecondPage
+class Page extends React.Component {
+
+  constructor(){
+    this.state = { msg: "waiting for server"};
+  }
+  
+  componentDidMount(){
+    fetch("./netlify/lambda/hello").then(res => res.json())
+    .then(json => this.setState({msg: json.msg}));
+  }
+
+  render() {
+    <div>
+      <h2>msg from server: {this.state.msg}</h2>
+      <SecondPage />
+    </div>
+  }
+}
+
+export default Page
