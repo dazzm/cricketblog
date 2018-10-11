66701bd1fd5c0b82c42562b70dcee1a76521665b
dazzm <mazam@pipingrock.com>
2018-10-11 23:12:35 +0530

    added fetch and calling jokes api

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,5 +1,9 @@
+import fetch from 'node-fetch'
 
 exports.handler = async (event, context) => {
-    let response = { statusCode: 200, body: JSON.stringify({ msg: "hello from server"}) }
+    const url = "https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke"
+    let res = await fetch(url)
+    let data = await res.json()
+    let response = { statusCode: 200, body: JSON.stringify({msg: "hi", ...data}) }
     return response
 }
