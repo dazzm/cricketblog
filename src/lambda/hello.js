73542869c8cf087fe028642b202a0f31d7e7fc2c
dazzm <mazam@pipingrock.com>
2018-10-12 02:02:55 +0530

    added default value to cache get call

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -7,7 +7,7 @@ cache.set("counter", counter, 5*60)
 exports.handler = async (event, context) => {
     const url = "https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke"
     let data = await ajax.getJson(url)
-    let cout = cache.get("counter")
+    let cout = cache.get("counter", 0)
     let response = { statusCode: 200, body: JSON.stringify({msg: msg+cout, ...data}) }
     return response
 }
