23ffd19af77b56248d3e6080e2ae1fea1ea76169
dazzm <mazam@pipingrock.com>
2018-10-12 01:52:31 +0530

    added simplecaching

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,10 +1,13 @@
+import cache from 'simplecaching'
 import ajax from './utils/index'
 
-let counter = 0, msg = "hello "
+let counter = 1, msg = "hello "
+cache.set("counter", counter, 5*60)
 
 exports.handler = async (event, context) => {
     const url = "https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke"
     let data = await ajax.getJson(url)
-    let response = { statusCode: 200, body: JSON.stringify({msg: msg+counter++, ...data}) }
+    let cout = cache.get("counter")
+    let response = { statusCode: 200, body: JSON.stringify({msg: msg+cout, ...data}) }
     return response
 }
