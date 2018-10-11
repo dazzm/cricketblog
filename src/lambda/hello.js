597686bda5c0d84dc045a9018081315183f67287
dazzm <mazam@pipingrock.com>
2018-10-12 00:13:51 +0530

    added counter test2

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,10 +1,10 @@
 import ajax from './utils/index'
 
-let counter = 0
+let counter = 0, msg = "hi "
 
 exports.handler = async (event, context) => {
     const url = "https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke"
     let data = await ajax.getJson(url)
-    let response = { statusCode: 200, body: JSON.stringify({msg: "hi: "+ ++counter, ...data}) }
+    let response = { statusCode: 200, body: JSON.stringify({msg: msg+counter++, ...data}) }
     return response
 }
