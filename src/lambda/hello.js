28178cca66e62edff419f013b8a7bafe1a335f0c
dazzm <mazam@pipingrock.com>
2018-10-12 01:00:18 +0530

    changed msg

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,6 +1,6 @@
 import ajax from './utils/index'
 
-let counter = 0, msg = "hi "
+let counter = 0, msg = "hello "
 
 exports.handler = async (event, context) => {
     const url = "https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke"
