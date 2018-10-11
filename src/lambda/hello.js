9b0a8015957b05658e7263ce683bbc9777f0396e
dazzm <mazam@pipingrock.com>
2018-10-12 02:26:35 +0530

    added axios data

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -6,7 +6,8 @@ cache.set("counter", counter, 5*60)
 
 exports.handler = async (event, context) => {
     const url = "https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke"
-    let data = await axios.get(url)
+    let res = await axios.get(url)
+    let data = res.data
     let cout = cache.get("counter", 0)
     let response = { statusCode: 200, body: JSON.stringify({msg: msg+cout, ...data}) }
     return response
