8978e0571d8d1274f27543da3989736b4fa05026
dazzm <mazam@pipingrock.com>
2018-10-12 17:01:08 +0530

    added mongoose model user

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -2,13 +2,12 @@ import cache from 'simplecaching'
 import axios from 'axios'
 
 let counter = 1, msg = "hello "
-cache.set("counter", counter, 5*60)
 
 exports.handler = async (event, context) => {
+    context.callbackWaitsForEmptyEventLoop = false;
     const url = "https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke"
     let res = await axios.get(url)
     let data = res.data
-    let cout = cache.get("counter", 0)
-    let response = { statusCode: 200, body: JSON.stringify({msg: msg+cout, ...data}) }
+    let response = { statusCode: 200, body: JSON.stringify({msg: msg+counter, ...data}) }
     return response
 }
